9531fd2ca2d0d6da62348f57ff7a76666e0e25a9
zimbatm <zimbatm@zimbatm.com>
2018-10-16 15:22:53 +0200

    null_resource: add external triggers
    This allows to reset the resource when the output of an arbitrary
    scripts changes.
    
    This is a great way to implement deployment-type resources.

--- null/resource.go
+++ null/resource.go
@@ -3,6 +3,9 @@ package null
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
 	"time"
 
 	"github.com/hashicorp/terraform/helper/schema"
@@ -24,17 +27,138 @@ func resource() *schema.Resource {
 				Optional: true,
 				ForceNew: true,
 			},
+			"external_trigger": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"command": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"interpreter": &schema.Schema{
+							Type:     schema.TypeList,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Optional: true,
+						},
+						"working_dir": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"environment": &schema.Schema{
+							Type:     schema.TypeMap,
+							Optional: true,
+						},
+						"result": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+							ForceNew: true,
+						},
+					},
+				},
+			},
 		},
 	}
 }
 
+func runCommand(data *schema.ResourceData) error {
+	command := data.Get("command").(string)
+	if command == "" {
+		return fmt.Errorf("local-exec provisioner command must be a non-empty string")
+	}
+
+	// Execute the command with env
+	environment := data.Get("environment").(map[string]interface{})
+
+	var env []string
+	for k := range environment {
+		entry := fmt.Sprintf("%s=%s", k, environment[k].(string))
+		env = append(env, entry)
+	}
+
+	// Execute the command using a shell
+	interpreter := data.Get("interpreter").([]interface{})
+
+	var cmdargs []string
+	if len(interpreter) > 0 {
+		for _, i := range interpreter {
+			if arg, ok := i.(string); ok {
+				cmdargs = append(cmdargs, arg)
+			}
+		}
+	} else {
+		if runtime.GOOS == "windows" {
+			cmdargs = []string{"cmd", "/C"}
+		} else {
+			cmdargs = []string{"/bin/sh", "-c"}
+		}
+	}
+	cmdargs = append(cmdargs, command)
+
+	workingdir := data.Get("working_dir").(string)
+
+	var cmdEnv []string
+	cmdEnv = os.Environ()
+	cmdEnv = append(cmdEnv, env...)
+
+	// Setup the command
+	cmd := exec.Command(cmdargs[0], cmdargs[1:]...)
+	// Dir specifies the working directory of the command.
+	// If Dir is the empty string (this is default), runs the command
+	// in the calling process's current directory.
+	cmd.Dir = workingdir
+	// Env specifies the environment of the command.
+	// By default will use the calling process's environment
+	cmd.Env = cmdEnv
+
+	// Start the command
+	result, err := cmd.Output()
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			if exitErr.Stderr != nil && len(exitErr.Stderr) > 0 {
+				return fmt.Errorf("failed to execute %q: %s", command, string(exitErr.Stderr))
+			}
+			return fmt.Errorf("command %q failed with no error message", command)
+		} else {
+			return fmt.Errorf("failed to execute %q: %s", command, err)
+		}
+	}
+	data.Set("result", result)
+
+	return nil
+}
+
+func runExternalTriggers(d *schema.ResourceData) error {
+	d2, ok := d.GetOk("external_trigger")
+	if !ok {
+		return nil
+	}
+
+	for _, triggerRaw := range d2.([]interface{}) {
+		trigger := triggerRaw.(*schema.ResourceData)
+		err := runCommand(trigger)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func resourceCreate(d *schema.ResourceData, meta interface{}) error {
+	err := runExternalTriggers(d)
+	if err != nil {
+		return err
+	}
+
 	d.SetId(fmt.Sprintf("%d", rand.Int()))
+
 	return nil
 }
 
 func resourceRead(d *schema.ResourceData, meta interface{}) error {
-	return nil
+	return runExternalTriggers(d)
 }
 
 func resourceDelete(d *schema.ResourceData, meta interface{}) error {
--- website/docs/resource.html.markdown
+++ website/docs/resource.html.markdown
@@ -51,7 +51,6 @@ a single action that affects them all. Due to the `triggers` map, the
 `null_resource` will be replaced each time the instance ids change, and thus
 the `remote-exec` provisioner will be re-run.
 
-
 ## Argument Reference
 
 The following arguments are supported:
@@ -59,6 +58,74 @@ The following arguments are supported:
 * `triggers` - (Optional) A map of arbitrary strings that, when changed, will
   force the null resource to be replaced, re-running any associated
 provisioners.
+* `external_trigger` - (Optional) Run an external command to determine if the
+  resource should be re-created. See [External Trigger](#external-triggers)
+  below for details.
+
+### External triggers
+
+~> **Warning** Terraform Enterprise does not guarantee availability of any
+particular language runtimes or external programs beyond standard shell
+utilities, so it is not recommended to use this provider within configurations
+that are applied within Terraform Enterprise.
+
+Each `external_command` is executed on resource creation and read. If the
+output changes it triggers the resource re-creation. This allows for advanced
+deployment scenarios.
+
+The following arguments are supported:
+
+* `command` - (Required) This is the command to execute. It can be provided as a relative path to the current working directory or as an absolute path. It is evaluated in a shell, and can use environment variables or Terraform variables.
+
+* `working_dir` - (Optional) If provided, specifies the working directory where command will be executed. It can be provided as as a relative path to the current working directory or as an absolute path. The directory must exist.
+
+* `interpreter` - (Optional) If provided, this is a list of interpreter arguments used to execute the command. The first argument is the interpreter itself. It can be provided as a relative path to the current working directory or as an absolute path. The remaining arguments are appended prior to the command. This allows building command lines of the form "/bin/bash", "-c", "echo foo". If interpreter is unspecified, sensible defaults will be chosen based on the system OS.
+
+* `environment` - (Optional) block of key value pairs representing the environment of the executed command. inherits the current process environment.
+
+### Example
+
+In the case where the trigger should only happen on arbitrary commands, it's
+also possible to use the `external_trigger` method.
+
+```hcl
+resource "google_compute_instance" "myhost" {
+  # ...
+}
+
+locals {
+  host = "${google_compute_instance.myhost.network_interface.0.network_ip}"
+}
+
+resource "null_resource" "nixos_deploy" {
+  triggers {
+    # check if it's a new instance
+    instance_id = "${google_compute_instance.myhost.instance_id}"
+  }
+
+  # check if the nix evaluation output has changed
+  external_trigger {
+    command = "nix-instantiate ${path.module}/files"
+  }
+
+  connection {
+    type  = "ssh"
+    host  = "${local.host}"
+    user  = "root"
+    agent = true
+  }
+
+  # check that SSH is working
+  provisioner "remote-exec" {
+    inline = ["uname -a"]
+  }
+
+  # the deploy command
+  provisioner "local-exec" {
+    command = "nixos-rebuild --target-host root@${local.host} --build-host root@${local.host} -I nixos-config=${path.module}/files/configuration.nix switch"
+  }
+}
+```
 
 ## Attributes Reference
 
@@ -67,3 +134,5 @@ The following attributes are exported:
 * `id` - An arbitrary value that changes each time the resource is replaced.
   Can be used to cause other resources to be updated or replaced in response
   to `null_resource` changes.
+
+For any `external_trigger` the command `output` is exported.
